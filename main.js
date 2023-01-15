ab435dfb5a8bbd8ccdcbf058ebb259ef3139eee2
Giuseppeworkjujujj <you@example.com>
2023-01-15 22:10:12 +0100

    terzo

---
+++ main.js
@@ -0,0 +1,15 @@
+Vue.config.devtools = true;
+Vue.config.debug = true;
+
+const app = new Vue (
+    {
+        el: "#root",
+        data: {
+          
+        },
+        methods: {
+       
+            }
+        }
+    
+)
